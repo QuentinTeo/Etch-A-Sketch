3eca30fc42d93cceb70c3edcc6a1e9335110212a
QuentinTeo <anonmango6767@gmail.com>
2025-06-24 07:26:39 +0000

    added eventlisteners to 'Etch' using the grid boxes

--- Jscript.js
+++ Jscript.js
@@ -19,12 +19,12 @@ grid.style.cssText =
 // squareDiv.style.height = 16px;
 // squareDiv.style.border = "thick solid black";
 
-
+//5.3% for 16 x 16 in flexbox
 const gridDimensions = 16 * 16;
 
 for(let i = 0; i < gridDimensions; i++){
     const gridBox = document.createElement("div");
-    gridBox.id = "hover";
+    gridBox.classList.add("hover");
     gridBox.style.cssText= "flex: 1 1 5.3%; border: 2px solid red; background-color: blue; id: hover";
     grid.appendChild(gridBox);
 
@@ -41,9 +41,19 @@ for(let i = 0; i < gridDimensions; i++){
 
 // container.appendChild(squareDiv);
 
+//Mouse EventListener
 
-const etch = document.querySelector("#hover");
+let etch = document.querySelectorAll(".hover");
 
-etch.addEventListener("mouseover", (e) => {
-    etch.style.backgroundColor = "black";
-})
+//for loop to add events for all gridboxes
+for(let i = 0;i < etch.length;i++){
+    etch[i].addEventListener("mouseover", (e) => {
+        etch[i].style.backgroundColor = "black";
+    })
+}
+
+// const etch = document.querySelector(".hover");
+
+// etch.addEventListener("mouseover", (e) => {
+//     etch.style.backgroundColor = "black";
+// })
